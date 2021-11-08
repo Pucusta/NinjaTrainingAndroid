1d4aacf5fc2b53065f5e542d8225b83f8946f544
Galatean Marcell <galateanmarcell96@gmail.com>
2021-11-08 14:30:22 +0100

    projekt létrehozása

---
+++ app/src/main/java/hu/bme/aut/ninjatraining/model/Position.kt
@@ -0,0 +1,6 @@
+package hu.bme.aut.ninjatraining.model
+
+data class Position(
+    var x: Float = 0F,
+    var y: Float = 0F
+)
---
+++ app/src/main/java/hu/bme/aut/ninjatraining/model/Stone.kt
@@ -0,0 +1,8 @@
+package hu.bme.aut.ninjatraining.model
+
+class Stone( var position: Position = Position(0F, 0F), var velocity: Float = 0F) {
+
+    fun applyAcceleration(acceleration: Float) {
+
+    }
+}
